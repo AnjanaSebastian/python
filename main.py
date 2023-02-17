c28143c8159e4ef4369ffe0d461834b38eb6664f
Anjana Sebastian <98539856+AnjanaSebastian@users.noreply.github.com>
2023-02-17 21:15:57 +0530

    student  profile

---
+++ main.py
@@ -0,0 +1,11 @@
+from class import Student
+
+student = Student(
+    name="Anjana Sebastian",
+    place="Alappuzha",
+    age=20
+)
+student.display_student_details()
+print("="*20)
+student.set_age(21)
+student.display_student_details()
